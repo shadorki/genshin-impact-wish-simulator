28fcd2026656df5dc36e08ba3567d65676890377
Uzair Ashraf <uzinatorcl@live.com>
2020-10-05 15:32:33 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, 'src');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  "entry": "./src",
+  "output": {
+    "path": __dirname + '/dist',
+    "filename": "main.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    watchContentBase: true,
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  "module": {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: srcPath,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+      }
+    ]
+  }
+};
