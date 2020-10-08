cb744261273eabb737cf7d19f0b250e75d6e58f8
Uzair Ashraf <uzinatorcl@live.com>
2020-10-07 22:52:33 -0700

    wanderlust invocation complete

--- src/components/wanderlust-invocation-details.jsx
+++ src/components/wanderlust-invocation-details.jsx
@@ -4,12 +4,12 @@ import DetailsBox from './details-components/details-box';
 import StarsBar from './details-components/stars-bar';
 import SubheadingSeparator from './details-components/subheading-separator';
 import EventDurationHeading from './details-components/event-duration-heading'
-import BalladsInGoblets from '../models/ballad-in-goblets'
+import WanderlustInvocation from '../models/wanderlust-invocation'
 import Table from './details-components/table'
 import Title from './details-components/title'
 
 export default function WanderlustInvocationDetails() {
-
+  const wanderlust = new WanderlustInvocation()
   return (
     <div className="details pt-5">
       <Container>
@@ -48,6 +48,30 @@ export default function WanderlustInvocationDetails() {
             </h4>
           </Col>
         </Row>
+        <StarsBar
+          starCount={5}
+          content="Base Probability for 5-Star Item Drops: 0.600% (Incl. guarantee: 1.600%)"
+          bgColor="#dcbba5"
+        />
+        <Table
+          items={wanderlust.getDrops(5)}
+        />
+        <StarsBar
+          starCount={4}
+          content="Base Probability for 4-Star Item Drops: 5.100% (Incl. guarantee: 13.000%)"
+          bgColor="#b6abbf"
+        />
+        <Table
+          items={wanderlust.getDrops(4)}
+        />
+        <StarsBar
+          starCount={3}
+          content="Base Probability for 3-Star Item Drops: 94.300% (Incl. guarantee: 85.400%)"
+          bgColor="#a5bacc"
+        />
+        <Table
+          items={wanderlust.getDrops(3)}
+        />
       </Container>
     </div>
   )
