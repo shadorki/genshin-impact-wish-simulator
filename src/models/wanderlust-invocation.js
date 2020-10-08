38c5fb8a76988bcacf6927b7e75da69701ed7bf2
Uzair Ashraf <uzinatorcl@live.com>
2020-10-07 22:46:17 -0700

    basic class added

---
+++ src/models/wanderlust-invocation.js
@@ -0,0 +1,8 @@
+import BaseGacha from './base-gacha'
+import drops from '../data/wanderlust-invocation.json'
+
+export default class WanderlustInvocation extends BaseGacha {
+  constructor() {
+    super(drops)
+  }
+}
