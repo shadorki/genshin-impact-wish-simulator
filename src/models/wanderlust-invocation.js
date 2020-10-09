8e319bac6a0c3c0c1fc883debb4a19bfd13f22bb
Uzair Ashraf <uzinatorcl@live.com>
2020-10-08 22:41:13 -0700

    wanderlust invocation class ready

--- src/models/wanderlust-invocation.js
+++ src/models/wanderlust-invocation.js
@@ -4,5 +4,49 @@ import drops from '../data/wanderlust-invocation.json'
 export default class WanderlustInvocation extends BaseGacha {
   constructor() {
     super(drops)
+    this.attemptsCount = 0;
+    this.guaranteed5Star = false
+    this.probabilityRange = this.generateProbabilityRange(943, 51, 6)
+  }
+  set attempts(amount) {
+    this.attemptsCount += amount
+    this.guaranteed5Star = !(this.attemptsCount % 90)
+  }
+  roll() {
+    const roll = []
+    this.shuffle(this.probabilityRange)
+    this.attempts = 10
+    // checks to see if 90 attempts have passed, and grabs 5 star item
+    if (this.guaranteed5Star) {
+      roll.push(this.getRandomItem(5))
+    }
+    // 4 star item or higher guaranteed every 10 rolls
+    roll.push(this.getGuaranteed4StarItemOrHigher())
+
+    const rollsToGo = 10 - roll.length
+
+    for (let i = 0; i < rollsToGo; i++) {
+      roll.push(this.rollBasedOffProbability())
+    }
+    return roll
+  }
+  rollBasedOffProbability() {
+    const itemRating = this.probabilityRange[this.generateRandomNumber(this.probabilityRange.length)]
+    return this.getRandomItem(itemRating)
+  }
+  getRandomItem(rating) {
+    const itemsList = this.getDrops(rating)
+    const item = itemsList[this.generateRandomNumber(itemsList.length)]
+    return item
+  }
+  getGuaranteed4StarItemOrHigher() {
+    // 12% chance of getting 5 star item
+    const twelveRandomNumbers = this.generateRandomNumbers(12, 100)
+    const randomNumber = this.generateRandomNumber(100)
+    const didUserGet5StarItem = twelveRandomNumbers.has(randomNumber)
+    if (didUserGet5StarItem) {
+      return this.getRandomItem(5)
+    }
+      return this.getRandomItem(4)
   }
 }
