954ca4fecea8c7c3391a943de2a6728fbe68f2d8
Uzair Ashraf <uzinatorcl@live.com>
2020-10-13 23:58:37 -0700

    user can now skip wish scene

--- src/components/wish.jsx
+++ src/components/wish.jsx
@@ -4,6 +4,10 @@ import gachaClip from '../assets/images/ui/gacha.mp4'
 export default function Wish(props) {
   const { setView } = props
   return (
+    <>
+    <button
+    onClick={() => setView('wish-results')}
+    className="skip-button">Skip</button>
     <video
     autoPlay={true}
     className="min-vh-100 w-100 overflow-hidden"
@@ -14,5 +18,6 @@ export default function Wish(props) {
         src={gachaClip}
         type="video/mp4"/>
     </video>
+    </>
   )
 }
