3f44bf1b10eb30d6c6c37c12573e1857ccd645e4
lauslim12 <nicholasdwiarto@yahoo.com>
2020-10-19 12:20:47 +0700

    add test for the second SSR, it is now Venti after a non-Venti SSR pull

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const chai = require('chai')
 const { expect } = chai
+import { assert } from 'chai'
 import BalladInGoblets from '../src/models/ballad-in-goblets'
 import BeginnersWish from '../src/models/beginners-wish'
 import EpitomeInvocation from '../src/models/epitome-invocation'
@@ -75,6 +76,49 @@ describe('Testing suite for genshin impact gacha', () => {
 
         // Then, if we get Venti in less than 180 pulls, set 'hasVenti' to true and exit the loop.
         if (results[i].find((item) => item.rating === 5 && item.name === 'Venti')) {
+          hasVenti = true;
+          break;
+        }
+      }
+
+      expect(hasVenti).to.be.true;
+      done();
+    })
+    it('should give a Venti after pulling an SSR that is not Venti (first Venti pull is also acceptable)', done => {
+      const results = [];
+      const balladVenti = new BalladInGoblets();
+      let hasVenti = false;
+      let hasSSR = false;
+
+      // Infinite loop, we want to keep pulling until we discovered a Venti.
+      while (true) {
+        results.push(balladVenti.roll());
+        
+        // Filter all the results by its five star.
+        // If there are any results, store its name in a new array for easier checking.
+        const filteredResults = results[results.length - 1].filter((item) => item.rating === 5);
+        const names = filteredResults.map((e) => e.name);
+        const areNamesFilled = names.length > 0 ? true : false;
+        
+        // This step will fail if the second SSR is not Venti.
+        // We'll also have to check if we pulled any SSR, hence the 'names.length' to prevent false negatives.
+        if (hasSSR && !names.includes('Venti') && areNamesFilled) {
+          assert.fail('The second SSR pulled was not Venti!');
+        }
+
+        // If the first SSR is not Venti, set 'hasSSR' to true.
+        if (!names.includes('Venti') && areNamesFilled) {
+          hasSSR = true;
+        }
+
+        // The next SSR, we have to check if it is truly Venti.
+        if (hasSSR && names.includes('Venti') && areNamesFilled) {
+          hasVenti = true;
+          break;
+        }
+
+        // If the SSR is Venti, then exit.
+        if (names.includes('Venti') && areNamesFilled) {
           hasVenti = true;
           break;
         }
