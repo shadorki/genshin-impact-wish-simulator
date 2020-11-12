cd76007e1aaf35efec8a1ffdfad2e6c947fad767
Uzair Ashraf <uzinatorcl@live.com>
2020-11-12 01:02:15 -0800

    wanderlust invocation details updated

--- src/components/wanderlust-invocation-details.jsx
+++ src/components/wanderlust-invocation-details.jsx
@@ -13,7 +13,7 @@ export default function WanderlustInvocationDetails() {
     <div className="details pt-5">
       <Container>
         <Title>
-          <h1>| Standard Wish "Wanderlust <span className="gold">Invocation</span>"</h1>
+          <h1>| Standard Wish "Wanderlust <span className="blue">Invocation</span>"</h1>
         </Title>
         <SubheadingSeparator
           content="Wish Details"
@@ -24,22 +24,22 @@ export default function WanderlustInvocationDetails() {
         <Row>
           <Col xs="12">
           <p className="my-3">
-            Wanderlust <span className="gold">Invocation</span> is a standard wish with no time limit. Non-promotional characters and weapons are available. <br/>
-            In this wish, each 10-wish set is <span className="orange">guaranteed</span> to include at least one 4-star or above item
+            Wanderlust <span className="blue">Invocation</span> is a standard wish with no time limit. Non-promotional characters and weapons are available. <br/>
+            In this wish, <span className="orange">guaranteed</span> to win 4-star or above item at least once per 10 attempts.
           </p>
           <p className="my-3">
             〓Rules〓
           </p>
           <p className="my-3">
-              Base probability of winning 5-star item = <span className="orange">0.600%</span>; consolidated probability (incl. guarantee) = <span className="orange">1.600%</span>; guaranteed to win 5-star item at least once per <span className="orange">90</span> attempts. <br/>
-              Base probability of winning 4-star item = <span className="orange">5.100%</span>; consolidated probability (incl. guarantee) = <span className="orange">13.000%</span>; guaranteed to win 4-star or above item at least once per <span className="orange">10</span> attempts. <br/>
-              5-star weapons won in this wish include <span className="gold">Masterless Starglitter</span> ×10; 4-star weapons include <span className="gold">Masterless Starglitter</span> ×2; 3-star weapons include <span className="purple">Masterless Stardust</span> ×15.
+            Base probability of winning 5-star item = <span className="orange">0.600%</span>; base probability of winning 5-star character = <span className="orange">0.300%</span>, and base probability of winning 5-star weapon = <span className="orange">0.300%</span>; consolidated probability (incl. guarantee) of winning 5-star item = 1.600%; guaranteed to win 5-star item at least once per <span className="orange">90</span> attempts.
+            Base probability of winning 4-star item = <span className="orange">5.100%</span>; base probability of winning 4-star character = <span className="orange">2.550%</span>, and base probability of winning 4-star weapon = <span className="orange">2.550%</span>; consolidated probability (incl. guarantee) of winning 4-star item = <span className="orange">13.000%</span>; guaranteed to win 4-star or above item at least once per <span className="orange">10</span> attempts. <br/>
+            5-star weapons won in this wish include <span className="orange">Masterless Starglitter</span> ×10; 4-star weapons include <span className="orange">Masterless Starglitter</span> ×2; 3-star weapons include <span className="purple">Masterless Stardust</span> ×15.
           </p>
             <p className="my-3">
               〓Duplicate Characters〓
             </p>
             <p className="my-3">
-              On obtaining a 5-star character that you already own (whether obtained in a wish, redeemed at the shop, or awarded by the game): The 2nd – 7th time you obtain the character, it will be converted into <span className="purple">that character's Stella Fortuna</span> ×1 and <span className="orange">Masterless Starglitter</span> ×10; from the 8th time onwards it will be converted into <span className="orange">Masterless Starglitter</span> ×25.
+              On obtaining a 5-star character that you already own (whether obtained in a wish, redeemed at the shop, or awarded by the game): The 2nd – 7th time you obtain the character, it will be converted into <span className="purple">that character's Stella Fortuna</span> ×1 and <span className="gold">Masterless Starglitter</span> ×10; from the 8th time onwards it will be converted into <span className="gold">Masterless Starglitter</span> ×25. <br/>
               On obtaining a 4-star character that you already own (whether obtained in a wish, redeemed at the shop, or awarded by the game): The 2nd – 7th time you obtain the character, it will be converted into <span className="purple">that character's Stella Fortuna</span> ×1 and <span className="gold">Masterless Starglitter</span> ×2; from the 8th time onwards it will be converted into <span className="gold">Masterless Starglitter</span> ×5.
             </p>
             <h4 className="my-3 brass">
