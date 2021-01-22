ae6612a905ee29db84f88cc5f18e263ef6f146b6
David Lefaive <david.lefaive@sjsu.edu>
2021-01-22 00:22:43 -0800

    Cleanup

--- src/models/sparkling-steps.js
+++ src/models/sparkling-steps.js
@@ -4,6 +4,5 @@ import drops from '../data/sparkling-steps.json'
 export default class SparklingSteps extends BaseGacha {
   constructor() {
     super(drops)
-    this.probabilityRange = this.generateProbabilityRange(943, 51, 6)
   }
 }
