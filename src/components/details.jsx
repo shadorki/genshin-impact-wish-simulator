6797ab2c9859e7fa013fa22e2d37b539d0866e85
Uzair Ashraf <uzinatorcl@live.com>
2021-02-06 16:18:06 -0800

    missed rename in details v1.3.0

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import AdriftInTheHarborDetails from './invitation-to-mundane-life-details'
+import InvitationToMundaneLifeDetails from './invitation-to-mundane-life-details'
 import EpitomeInvocationDetails from './epitome-invocation-details'
 import Navbar from './details-components/navbar'
 import WanderlustInvocationDetails from './wanderlust-invocation-details'
@@ -8,7 +8,7 @@ import BeginnersWishDetails from './beginners-wish-details'
 export default function Details(props) {
   const { selectedDetail, backToHome } = props
   const pages = {
-    'invitation-to-mundane-life': <AdriftInTheHarborDetails/>,
+    'invitation-to-mundane-life': <InvitationToMundaneLifeDetails/>,
     'epitome-invocation': <EpitomeInvocationDetails/>,
     'wanderlust-invocation': <WanderlustInvocationDetails/>,
     'beginners-wish': <BeginnersWishDetails/>
