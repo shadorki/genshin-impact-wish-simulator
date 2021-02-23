d22e484cf001c69823b9e5dcefcf545f6e98f070
Uzair Ashraf <uzinatorcl@gmail.com>
2021-02-22 21:05:55 -0800

    brought in form stling from react strap

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -1,35 +1,70 @@
 import React, { Component } from 'react'
+import { Container, Row, Col, Badge, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-export default function Settings(props) {
-  const {
-    reset,
-    selectedCharacterEventWish,
-    updateCharacterEventWish,
-    closeSettings
-  } = props
-  console.log({
-    reset,
-    selectedCharacterEventWish,
-    updateCharacterEventWish,
-    closeSettings
-  })
-  return (
-    <div
-    onClick={closeSettings}
-    className="modal-container">
+export default class Settings extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      characterBanner: this.props.selectedCharacterEventWish,
+      language: 'English'
+    }
+    this.onChange = this.onChange.bind(this)
+  }
+  onChange({ target: { name, value } }) {
+    this.setState({ [name]: value })
+  }
+  render() {
+    const {
+      reset,
+      selectedCharacterEventWish,
+      updateCharacterEventWish,
+      closeSettings
+    } = this.props
+    console.log({
+      reset,
+      selectedCharacterEventWish,
+      updateCharacterEventWish,
+      closeSettings
+    })
+    return (
       <div
-      onClick={e => e.stopPropagation()}
-      className="settings-modal">
-        <div className="settings-modal-content p-2">
-          <div
-          onClick={reset}
-          className="close-button"></div>
-          <h2>Settings</h2>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto incidunt rem similique consequatur dicta aspernatur vero. Aspernatur quos non neque velit sequi, iusto officia ab omnis eligendi? Non, distinctio quaerat.
-          </p>
+        onClick={closeSettings}
+        className="modal-container">
+        <div
+          onClick={e => e.stopPropagation()}
+          className="settings-modal">
+          <div className="settings-modal-content p-2">
+            <div
+              onClick={closeSettings}
+              className="close-button"></div>
+            <h2>Settings</h2>
+            <Form>
+              <Container>
+                <Row>
+                  <Col xs="12">
+                  <FormGroup>
+                    <Label for="characterBanner">Character Banner</Label>
+                    <Input
+                      type="select"
+                      name="characterBanner"
+                      id="characterBanner"
+                      onChange={this.onChange}
+                    >
+                      <option value="all">All</option>
+                      <option value="characters">Characters</option>
+                      <option value="weapons">Weapons</option>
+                      <option value="fiveStars">5 Stars</option>
+                      <option value="fourStars">4 Stars</option>
+                      <option value="threeStars">3 Stars</option>
+                    </Input>
+                  </FormGroup>
+                  </Col>
+                </Row>
+              </Container>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
