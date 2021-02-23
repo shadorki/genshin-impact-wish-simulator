132c6d5a283e98dd431d24e332fb9e64cd631dcd
Uzair Ashraf <uzinatorcl@gmail.com>
2021-02-22 20:47:59 -0800

    base layout for settings page added

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -3,13 +3,33 @@ import React, { Component } from 'react'
 export default function Settings(props) {
   const {
     reset,
-    setCharacterEventWish,
-    closeSettingsPage
+    selectedCharacterEventWish,
+    updateCharacterEventWish,
+    closeSettings
   } = props
   console.log({
     reset,
-    setCharacterEventWish,
-    closeSettingsPage
+    selectedCharacterEventWish,
+    updateCharacterEventWish,
+    closeSettings
   })
-  return 'Hello world'
+  return (
+    <div
+    onClick={closeSettings}
+    className="modal-container">
+      <div
+      onClick={e => e.stopPropagation()}
+      className="settings-modal">
+        <div className="settings-modal-content p-2">
+          <div
+          onClick={reset}
+          className="close-button"></div>
+          <h2>Settings</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto incidunt rem similique consequatur dicta aspernatur vero. Aspernatur quos non neque velit sequi, iusto officia ab omnis eligendi? Non, distinctio quaerat.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
