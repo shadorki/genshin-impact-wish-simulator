45724435bc60f1df5c53f71f3c6837f371062c24
Uzair Ashraf <uzinatorcl@gmail.com>
2021-04-17 18:04:22 -0700

    New character event wish updates settings and app page

--- utils/new-character-event-wish.js
+++ utils/new-character-event-wish.js
@@ -8,12 +8,40 @@ let { previous, next } = require('minimist')(process.argv.slice(2));
 
 const shouldSkipCapitalization = (s, i) => !!i && new Set(['in', 'the', 'and', 'of', 'for']).has(s)
 
+const doesStillNeedReplacing = str => Object.values(previous).some(v => str.includes(v))
+
 const formatBanners = banner => ({
   kebab: banner,
   display: banner.split('-').map((s, i) => shouldSkipCapitalization(s, i) ? s : s[0].toUpperCase() + (!!s[1] ? s.slice(1) : '')).join(' '),
   pascal: banner.split('-').map(s => s[0].toUpperCase() + (!!s[1] ? s.slice(1) : '')).join(''),
   camel: banner.split('-').map((s, i) => !i ? s : s[0].toUpperCase() + (!!s[1] ? s.slice(1) : '')).join('')
 })
+
+const replaceAllInstancesInFile = (filePath, previous, next) => {
+  const p = path.join(__dirname, filePath)
+  let file = fs.readFileSync(p).toString()
+  for (const key in previous) {
+    file = file.replace(previous[key], next[key])
+  }
+  fs.writeFileSync(p, file)
+}
+
+const appendAllInstancesInFile = (file, previous, next) => {
+      const newFile = []
+      file.forEach((a, i) => {
+        newFile.push(a)
+        if (doesStillNeedReplacing(a)) {
+          let str = a
+          while (doesStillNeedReplacing(str)) {
+            for (const key in previous) {
+              str = str.replace(previous[key], next[key])
+            }
+          }
+          newFile.push(str)
+        }
+      })
+      return newFile
+}
 
 
   ; (async () => {
@@ -40,14 +68,51 @@ const formatBanners = banner => ({
 
       testFiles.forEach(f => {
         const filePath = '../cypress/integration/' + f
-        let test = fs.readFileSync(path.join(__dirname, filePath)).toString()
-        for( const key in previous ) {
-          test = test.replace(previous[key], next[key])
-        }
-        fs.writeFileSync(filePath, test)
+        replaceAllInstancesInFile(filePath, previous, next)
       })
 
-      console.log(testFiles)
+      // Create a new model for banner
+      const modelPath = path.join(__dirname, '../src/models/')
+      const oldModelPath = modelPath + previous.kebab + '.js'
+      const newModelPath = modelPath + next.kebab + '.js'
+
+      let modelFile = fs.readFileSync(oldModelPath).toString()
+      modelFile = modelFile.replace(previous.kebab, next.kebab)
+      modelFile = modelFile.replace(previous.pascal, next.pascal)
+
+      fs.writeFileSync(newModelPath, modelFile)
+
+      // Update the apps page
+      const appPath = path.join(__dirname, '../src/components/app.jsx')
+      const appFile = fs.readFileSync(appPath).toString().split('\n')
+      // Change selected character event wish
+      console.log('selectedCharacterEventWish: ' + previous.kebab + ',')
+      const selectedCharacterEventWishIndex = appFile.findIndex(s => s.includes("selectedCharacterEventWish: '" + previous.kebab + "',"))
+      if (selectedCharacterEventWishIndex === -1) {
+        console.log('Appfile has messed up selected character event wish, or it does not currently match the previous banner')
+        process.exit(1)
+      }
+      appFile[selectedCharacterEventWishIndex] = appFile[selectedCharacterEventWishIndex].replace(previous.kebab, next.kebab)
+
+      // Append the changes to whereever it's needed
+      const newAppfile = appendAllInstancesInFile(appFile, previous, next)
+
+      fs.writeFileSync(appPath, newAppfile.join('\n'))
+
+      //Update the settings page
+
+      const settingsPath = path.join(__dirname, '../src/components/settings.jsx')
+      const settingsFile = fs.readFileSync(settingsPath).toString().split('\n')
+      const newSettingsfile = appendAllInstancesInFile(settingsFile, previous, next)
+      fs.writeFileSync(settingsPath, newSettingsfile.join('\n'))
+
+      // Update the details page
+      const detailsPath = path.join(__dirname, '../src/components/details.jsx')
+      const detailsFile = fs.readFileSync(detailsPath).toString().split('\n')
+      const newDetailsfile = appendAllInstancesInFile(detailsFile, previous, next)
+      fs.writeFileSync(detailsPath, newDetailsfile.join('\n'))
+
+
     } catch (err) {
       console.error(err)
       process.exit(1)
